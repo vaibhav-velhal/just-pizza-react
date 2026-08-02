a098bb74cd4244b42e75fe8a19fe144fb5a9d393
vaibhav-velhal <vaibhav.velhal1614@gmail.com>
2026-08-02 16:53:51 +0530

    header image height adjusted and image added at about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,15 +6,41 @@ function About() {
     <div className="about container text-center">
       <h1 className="fw-normal">About Us</h1>
       <hr />
-      <p className="px-3">
-        At JustPizza, we believe great pizza starts with quality ingredients
-        and a passion for flavor. Every slice we serve is crafted with fresh
-        dough, rich sauces, and premium toppings to deliver an unforgettable
-        taste. Whether you prefer classic flavors or adventurous combinations,
-        our menu has something for everyone. We aim to provide delicious food,
-        quick service, and a warm experience that keeps you coming back for
-        more.
-      </p>
+      {/* Design 1 */}
+      {/* <div className="about-content row">
+        <div className="col-12 col-md-6 d-flex align-items-center">
+          <div className="image-container rounded-4">
+            <img src={MultiplePizzas} alt="about-image" />
+          </div>
+        </div>
+        <div className="col-12 col-md-6 d-flex align-items-center">
+          <p className="about-desc px-3 rounded-4">
+            At JustPizza, we believe great pizza starts with quality ingredients
+            and a passion for flavor. Every slice we serve is crafted with fresh
+            dough, rich sauces, and premium toppings to deliver an unforgettable
+            taste. Whether you prefer classic flavors or adventurous combinations,
+            our menu has something for everyone. We aim to provide delicious food,
+            quick service, and a warm experience that keeps you coming back for
+            more.
+          </p>
+        </div>
+      </div> */}
+      
+      {/* Design 2 */}
+      <div className="about-content d-flex justify-content-center">
+          <div className="image-container mt-3 rounded-4">
+            <img src={MultiplePizzas} alt="about-image" />
+            <p className="about-desc pb-0 pb-md-2 px-3 mx-3 rounded-4">
+              At JustPizza, we believe great pizza starts with quality ingredients
+              and a passion for flavor. Every slice we serve is crafted with fresh
+              dough, rich sauces, and premium toppings to deliver an unforgettable
+              taste. Whether you prefer classic flavors or adventurous combinations,
+              our menu has something for everyone. We aim to provide delicious food,
+              quick service, and a warm experience that keeps you coming back for
+              more.
+            </p>
+          </div>
+      </div>
     </div>
   );
 }
