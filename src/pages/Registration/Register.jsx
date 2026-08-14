1415b77ef8c03176020e62ac2176af78445394b8
vaibhav-velhal <vaibhav.velhal1614@gmail.com>
2026-08-14 10:08:28 +0530

    renamed to Registration.css during importing

--- src/pages/Registration/Register.jsx
+++ src/pages/Registration/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { registerUser } from "../../services/auth/auth.api.js";
-import "./register.css";
+import "./Register.css";
 
 
 function Register() {
