0ddf87bae4c971ed76cbbea25e33d52748a8e698
vaibhav-velhal <vaibhav.velhal1614@gmail.com>
2026-08-15 17:51:16 +0530

    No Page found route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import { Routes, Route } from "react-router-dom";
-import Login from './pages/Login/Login';
+import Login from "./pages/Login/Login";
 import Register from "./pages/Registration/Register";
-import Home from './pages/Home/Home';
+import Home from "./pages/Home/Home";
 
 function App() {
-
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route
+          path="*"
+          element={
+            <div>
+              <h2 className="text-center mt-3">404 - No page found</h2>
+            </div>
+          }
+        />
       </Routes>
     </>
-  )
+  );
 }
 
 export default App;
