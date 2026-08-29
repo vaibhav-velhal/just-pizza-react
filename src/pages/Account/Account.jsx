c102dd26ed0c5a244b54e63c5b4b5caf5e8b4248
vaibhav-velhal <vaibhav.velhal1614@gmail.com>
2026-08-29 17:37:49 +0530

    Style added for Profile section at Account page as well as fallback value is added for user data

--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -3,48 +3,85 @@ import Footer from './../../components/Footer/Footer';
 import { IoPerson } from 'react-icons/io5';
 import { useEffect, useState } from 'react';
 import { userDetails } from '../../services/user/user.api';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import { RiEdit2Fill } from "react-icons/ri";
+import "./Account.css"
 
 function Account() {
 
-    const [user, setUser] = useState([]);
+    const [userData, setUserData] = useState({});
     const token = JSON.parse(localStorage.getItem("token"));
     const params = useParams();
     const userId = params.userId;
-    
+
     useEffect(function(){
-        const getUser = async () => {
-            const res = await userDetails(token, userId);
-            setUser(res);
-        };
-
-        getUser();
+        try{
+            const getUser = async () => {
+                const res = await userDetails(token, userId);
+                
+                setUserData(res);
+            };
+    
+            getUser();
+        } catch(error) {
+            console.error(error);
+        }
     }, []);
-
+    
     return(
         <>
             <header>
                 <Navbar />
             </header>
             <main>
-                <div className="container text-center p-3" style={{marginTop: "80px", minHeight : "100vh"}}>
-                    <h1 className='fs-3'><IoPerson className='mb-2' /> Profile</h1><hr />
+                <div className="container account-page text-light p-3 rounded-4">
                     { 
                         token ?
                             (
                                 <>
                                     <div className="user-details">
-                                        <p>First Name: {user.firstName}</p>
-                                        <p>Last Name: {user.lastName}</p>
-                                        <p>Email: {user.email}</p>
-                                        <p>Age: {user.age}</p>
-                                        <p>Country: {user.country}</p>
+                                        <h1 className='text-center fs-3'><IoPerson className='mb-2' /> Profile</h1>
+                                        <div className="card border-light shadow rounded-4">
+                                            <div className="card-body text-start">
+                                                <p>First Name: {" "}
+                                                {
+                                                    !userData ? (<span>Loading...</span>) : (<span>{userData.firstName}</span>)
+                                                }
+                                                </p>
+                                                <p>Last Name: {" "}
+                                                {
+                                                    !userData ? (<span>Loading...</span>) : (<span>{userData.lastName}</span>)
+                                                }
+                                                </p>
+                                                <p>Email: {" "}
+                                                {
+                                                    !userData ? (<span>Loading...</span>) : (<span>{userData.email}</span>)
+                                                }
+                                                </p>
+                                                <p>Age: {" "}
+                                                {
+                                                    !userData ? (<span>Loading...</span>) : (<span>{userData.age}</span>)
+                                                }
+                                                </p>
+                                                <p>Country: {" "}
+                                                {
+                                                    !userData ? (<span>Loading...</span>) : (<span>{userData.country}</span>)
+                                                }
+                                                </p>
+                                                <div className="btn-section text-center pt-2">
+                                                    <Link to="/" className='edit-btn text-light text-decoration-none'><RiEdit2Fill size={"1.15rem"} className='mb-1'/>Edit</Link>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </>
                             ) :
                             (
                                 <>
-                                    <p>Please login <a href="/login">Login here</a></p>                                    
+                                    <div className="container text-center">
+                                        <p className='m-0'>You are not Logged-in</p>
+                                        <a href="/login"><small>Please login here</small></a>
+                                    </div>
                                 </>
                             )
                     }
