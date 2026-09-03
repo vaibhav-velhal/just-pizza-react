1a43165e4c6391a7ad4ae281bb0a02a3283d7623
vaibhav-velhal <vaibhav.velhal1614@gmail.com>
2026-09-03 13:54:56 +0530

    Header image is updated at Home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,13 @@ function Home() {
 
     return(
         <div id="header" className="header header-container">
-            <div className="welcome-text">
-                <h1 className='m-0 fw-bold'>Welcome to JustPizza</h1>
-                <p className='m-0 fw-light'>A Slice for Everyone</p>
+            <div className="container header-text">
+                <h1 className='m-0 text-light fw-bold'>
+                    Hot <span className='header-small-text'>Pizza.</span> <br />
+                    <span className='header-small-text'>Great Taste.</span> <br />
+                    Delivered Fast.
+                </h1>
+                <p className='text-secondary'>Delicious pizzas made with fresh ingredients and love. Order now and enjoy!</p>
             </div>
         </div>
     )
