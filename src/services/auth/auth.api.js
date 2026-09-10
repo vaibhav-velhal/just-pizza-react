fde57236658595eac514adb965413fcf6182b450
vaibhav-velhal <vaibhav.velhal1614@gmail.com>
2026-09-10 20:45:14 +0530

    Unnecessary error-handling removed in auth api module

--- src/services/auth/auth.api.js
+++ src/services/auth/auth.api.js
@@ -2,45 +2,42 @@ const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 // Login User
 export const loginUser = async (email, password) => {
-    try{
-        const res = await fetch(`${BASE_URL}/api/auth/login`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                email, password
-            })
-        });
-        
-        const data = await res.json();
-
-        return data;
-
-    } catch(error) {
-        throw error;
+    const res = await fetch(`${BASE_URL}/api/auth/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            email, password
+        })
+    });
+    
+    const data = await res.json();
+    
+    if (!res.ok) {
+        throw new Error(data.msg || "Failed to fetch data");
     }
+    
+    return data;
 }
 
 
 // Register User
-export const registerUser = async (userData) => {
-    try {
-        const res = await fetch(`${BASE_URL}/api/auth/registration`, {
-            method: "POST",
-            headers: {
-                "content-type": "application/json"
-            },
-            body: JSON.stringify(userData)
-        });
-        
-        if (!res.ok) {
-            throw new Error("Registration failed");
-        }
 
-        return res.json()
-    } catch(error) {
-        throw error;
+export const registerUser = async (userData) => {
+    const res = await fetch(`${BASE_URL}/api/auth/registration`, {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(userData)
+    });
+
+    const data = await res.json();
+
+    if (!res.ok) {
+        throw new Error(data.msg || "Registration failed");
     }
 
+    return data;
 };
